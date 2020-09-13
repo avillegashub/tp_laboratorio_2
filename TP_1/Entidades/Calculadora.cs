81893bcee5df1f5dac40ba1c5773e339207fd5f3
Alvaro Villegas <correodebitter@gmail.com>
2020-09-13 16:51:46 -0300

    TP1

---
+++ TP_1/Entidades/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    static class Calculadora
+    {
+        
+        private static char ValidarOperador(char operador)
+        {
+            return operador;
+        }
+
+        private static double Operar(double num1, double num2, string operador)
+        {
+            return 0;
+        }
+    }
+}
